e0f62681ac70fb50c073c10808cd8ef1bfac9096
unknown <mchaperc@yahoo.com>
2015-02-14 18:17:50 -0500

    Help on AJAX

--- js/app.js
+++ js/app.js
@@ -3,24 +3,18 @@
 		$('#term-query').submit(function(event){
 			var params = {
 				like: $('#like').val(),
-				countries: $('#countries').val(),
-				dateFrom: $('#dateFrom').val(),
-				dateTo: $('#dateTo').val(),
 				key: 'TTeZJQPWkFvyyXVB'
 			}
-				$.ajax({
+			
+			var result = $.ajax({
 					type: 'GET',
 					url: 'trendyskills.com/service?q=',
-					data: params,
-					contentType: "application/json; charset=utf-8",
-					dataType: "json",
-					success: function(data) {
-						console.log(data);
-					},
-					fail: function(error) {
-						console.log("error: " + error);
-					}
+					dataType: 'jsonp',
+					data: params
 				});
+
+			console.log(result);
+
 			console.log(params);
 		event.preventDefault();
 		});
