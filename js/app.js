ab2f300eb46523f5568eed9f340978e0d37cef10
mchaperc <mchaperc@yahoo.com>
2015-02-15 19:06:11 -0500

    ajax works, now layout

--- js/app.js
+++ js/app.js
@@ -4,9 +4,7 @@
 			var params = {
 				like: $('#like').val(),
 				key: 'TTeZJQPWkFvyyXVB'
-			}
-
-			alert(params.like);
+			};
 
 			var url = 'http://trendyskills.com/service?q=keywords&like=' + params.like + '&key=' + params.key;
 			
@@ -14,6 +12,73 @@
 			  url: url,
 			  // success: success,
 			  dataType: 'jsonp'
+			});
+
+			var job1 = results.keywords[0];
+			alert(results.id);
+			var job2 = results.responseJSON.keywords[1];
+			var job3 = results.responseJSON.keywords[2];
+			var job4 = results.responseJSON.keywords[3];
+			var job5 = results.responseJSON.keywords[4];
+
+			$(function () {
+
+			    // Radialize the colors
+			    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+			        return {
+			            radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+			            stops: [
+			                [0, color],
+			                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+			            ]
+			        };
+			    });
+
+			    // Build the chart
+			    $('.results').highcharts({
+			        chart: {
+			            plotBackgroundColor: null,
+			            plotBorderWidth: null,
+			            plotShadow: false
+			        },
+			        title: {
+			            text: 'Jobs meeting the following keyword: ' + params.like
+			        },
+			        tooltip: {
+			            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+			        },
+			        plotOptions: {
+			            pie: {
+			                allowPointSelect: true,
+			                cursor: 'pointer',
+			                dataLabels: {
+			                    enabled: true,
+			                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+			                    style: {
+			                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+			                    },
+			                    connectorColor: 'silver'
+			                }
+			            }
+			        },
+			        series: [{
+			            type: 'pie',
+			            name: 'Jobs',
+			            data: [
+			                ['Firefox',   45.0],
+			                ['IE',       26.8],
+			                {
+			                    name: 'Chrome',
+			                    y: 12.8,
+			                    sliced: true,
+			                    selected: true
+			                },
+			                ['Safari',    8.5],
+			                ['Opera',     6.2],
+			                ['Others',   0.7]
+			            ]
+			        }]
+			    });
 			});
 
 			console.log(results);
